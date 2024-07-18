3669b9875c0749130af0edadfde5f9944026921e
CJ Valentine <chijiokennabueze@gmail.com>
2024-07-18 01:22:47 +0100

    Update content_based.py

--- recommenders/content_based.py
+++ recommenders/content_based.py
@@ -30,7 +30,6 @@
 # Script dependencies
 import os
 import pandas as pd
-import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 
